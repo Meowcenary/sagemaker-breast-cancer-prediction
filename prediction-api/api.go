d40c0e6b52f637641fc9471968fe02e4d8ebb317
Eric Newman <newmaneric5@gmail.com>
2025-03-01 16:54:18 -0600

    Order Query Params for SageMaker Requests
    - Update API to order query params and set default values for
      requests sent to SageMaker
    - Update API to properly parse responses from SageMaker and respond with
      JSON

--- prediction-api/api.go
+++ prediction-api/api.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -16,21 +17,73 @@ import (
 	"github.com/gorilla/mux"
 )
 
+
+func orderFeatures(queryParams url.Values) []string {
+	// TODO This should probably be updated to the median value for each feature,
+	// but for now, using 0.0 will have to do
+	featureOrder := map[string]string{
+		"mean":                    "0.0",
+		"radius":                  "0.0",
+		"mean_texture":            "0.0",
+		"mean_perimeter":          "0.0",
+		"mean_area":               "0.0",
+		"mean_smoothness":         "0.0",
+		"mean_compactness":        "0.0",
+		"mean_concavity":          "0.0",
+		"mean_concave_points":     "0.0",
+		"mean_symmetry":           "0.0",
+		"mean_fractal_dimension":  "0.0",
+		"radius_error":            "0.0",
+		"texture_error":           "0.0",
+		"perimeter_error":         "0.0",
+		"area_error":              "0.0",
+		"smoothness_error":        "0.0",
+		"compactness_error":       "0.0",
+		"concavity_error":         "0.0",
+		"concave_points_error":    "0.0",
+		"symmetry_error":          "0.0",
+		"fractal_dimension_error": "0.0",
+		"worst_radius":            "0.0",
+		"worst_texture":           "0.0",
+		"worst_perimeter":         "0.0",
+		"worst_area":              "0.0",
+		"worst_smoothness":        "0.0",
+		"worst_compactness":       "0.0",
+		"worst_concavity":         "0.0",
+		"worst_concave_points":    "0.0",
+		"worst_symmetry":          "0.0",
+		"worst_fractal_dimension": "0.0",
+		"target":                  "0.0",
+	}
+
+	var record []string
+	for _, feature := range featureOrder {
+		if val, exists := queryParams[feature]; exists {
+			// Use provided value
+			record = append(record, val[0])
+		} else {
+			 // Use default value
+			record = append(record, featureOrder[feature])
+		}
+	}
+
+	return record
+}
+
 func predictHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse query parameters - this assumes feature values are passed as `?feature1=val1&feature2=val2...)
+	// Get the query params
 	queryParams := r.URL.Query()
+
 	// should have at least one param
 	if len(queryParams) == 0 {
 		http.Error(w, "Missing query parameters", http.StatusBadRequest)
 		return
 	}
 
-	// Convert query parameters to a CSV row
-	var record []string
-	for _, values := range queryParams {
-    	// Get the first value for each parameter
-		record = append(record, values[0])
-	}
+	// Parse query parameters to CSV row - this assumes feature values are
+	// passed as `?feature1=val1&feature2=val2...) where feature is subbed out for
+	// values in var featureOrder
+	var record = orderFeatures(queryParams)
 
 	// Convert to CSV format
 	var buf bytes.Buffer
@@ -52,8 +105,8 @@ func predictHandler(w http.ResponseWriter, r *http.Request) {
 	// Create SageMaker runtime client
 	sageMakerClient := sagemakerruntime.NewFromConfig(cfg)
 
-	endpointName := "sagemaker-xgboost-2025-02-23-22-17-09-203"
 	// Call SageMaker endpoint - need to use text/csv because that is what it was trained with
+	endpointName := "sagemaker-xgboost-2025-02-23-22-17-09-203"
 	resp, err := sageMakerClient.InvokeEndpoint(context.Background(), &sagemakerruntime.InvokeEndpointInput{
 		EndpointName: &endpointName,
 		Body:         buf.Bytes(),
@@ -64,25 +117,14 @@ func predictHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Read and decode SageMaker response
-	var result map[string]interface{}
-	err = json.NewDecoder(bytes.NewReader(resp.Body)).Decode(&result)
-	if err != nil {
-	    http.Error(w, "Failed to parse response from SageMaker", http.StatusInternalServerError)
-	    return
+	// Convert response body to a string
+	predictionStr := string(resp.Body)
+	if len(predictionStr) == 0 {
+		http.Error(w, "Empty response from SageMaker", http.StatusInternalServerError)
+		return
 	}
 
-	// Extract the prediction value (assuming it is inside a key like "predictions")
-	prediction, ok := result["predictions"]
-	if !ok {
-	    http.Error(w, "Prediction key not found in response", http.StatusInternalServerError)
-	    return
-	}
-
-	// Convert prediction to string
-	predictionStr := fmt.Sprintf("%v", prediction)
-
-	// Send response back to client
+	// Return prediction value as JSON
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"prediction": predictionStr})
 }
